4aa2bce5229fca12f00f083c985a114753e3397f
Sefhi <echo.mascarell@gmail.com>
2023-10-29 21:09:20 +0100

    updated rapi bot timezone message for daily reset

--- discord.js
+++ discord.js
@@ -40,6 +40,7 @@ const randomRapiMessages = [
 	`Mast? Who's that? Doesn't ring a bell.`,
 	`Commander! Yan sold me this swimsuit, what do you think about it? Here's a picture https://media.discordapp.net/attachments/1054761762395799552/1142732669617184898/image.png?width=445&height=595`,
 	`Commander, did you really tackle Crow? How did you do it?`,
+	`Age is just a number? Commander, I'm calling ACPU`,
 	`What do you mean my voice sounds similar to someone else? Who are you thinking about Commander? sigh...`,
 	`https://media.discordapp.net/attachments/1075785251156144179/1142745671766638592/1691823770699829.png`,
 ]
@@ -388,7 +389,7 @@ function initDiscordBot() {
 
 	// Daily message on reset time telling people what the current special interception is
 	let interceptionMessage = new CronJobb(
-		'0 22 * * *', () => {
+		'0 21 * * *', () => {
 			let guild = bot.guilds.cache.get('1054761356416528475')
 			const channel = guild.channels.cache.find(ch => ch.name === 'nikke')
 			if (!channel) return
