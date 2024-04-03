75cc9410f2f4adb76d7f19cd02f7e39094eb0b32
James Abraham <Jamesabraha@gmail.com>
2024-04-02 22:49:00 -0600

    wip: Update command functionality

--- commands/daily.js
+++ commands/daily.js
@@ -21,30 +21,23 @@ module.exports = {
             subcommand
                 .setName("time")
                 .setDescription(
-                    "Checks the time until the next daily reset (global server) for the specified game."
+                    "Checks the time until the next daily reset for the specified game."
                 )
                 .addStringOption((option) =>
                     option
                         .setName("game")
                         .setDescription("The name of the game")
                         .setRequired(false)
+                        .setAutocomplete(true)
                 )
         ),
     async execute(interaction) {
-        let gameName = interaction.options.getString("game") || "nikke";
-        let gameData;
-
-        if (gameName.toLowerCase() === "nikke") {
-            gameData = gamesData.find(
-                (game) =>
-                    game.game.toUpperCase() === "GODDESS OF VICTORY: NIKKE"
-            );
-        } else {
-            // Partial matching for other games
-            gameData = gamesData.find((game) =>
-                game.game.toUpperCase().includes(gameName.toUpperCase())
-            );
-        }
+        const gameName =
+            interaction.options.getString("game", false)?.toLowerCase() ||
+            "nikke";
+        const gameData = gamesData.find((game) =>
+            game.game.toLowerCase().includes(gameName)
+        );
 
         if (!gameData) {
             await interaction.reply({
@@ -54,19 +47,28 @@ module.exports = {
             return;
         }
 
-        const now = moment().tz(gameData.timezone);
+        const now = moment.tz(gameData.timezone);
         let resetTime = moment.tz(
             `${moment().format("YYYY-MM-DD")} ${gameData.dailyReset}`,
             gameData.timezone
         );
-        if (now > resetTime) resetTime.add(1, "days"); // Use next day's reset time if today's has passed
+        if (now > resetTime) {
+            resetTime.add(1, "days");
+        }
 
-        // Convert reset time to Unix timestamp and format it for Discord
         const resetTimestamp = `<t:${resetTime.unix()}:R>`;
 
         await interaction.reply({
             content: `The next reset for **${gameData.game}** (${gameData.server} Server) is ${resetTimestamp}.`,
             ephemeral: true,
         });
+    },
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused().toLowerCase();
+        const filtered = gamesData
+            .filter((game) => game.game.toLowerCase().includes(focusedValue))
+            .map((game) => ({ name: game.game, value: game.game }));
+
+        await interaction.respond(filtered.slice(0, 25)); // Limit to 25 results as per Discord's limit
     },
 };
