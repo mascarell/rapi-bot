6759ae10eb0deaa6115aa1ff06e250a65ee5a4b6
James Abraham <Jamesabraha@gmail.com>
2024-04-14 11:50:55 -0600

    task: Add ephemeral tags

--- commands/compositions.js
+++ commands/compositions.js
@@ -5,7 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('compositions')
         .setDescription('Get help for NIKKE team compositions'),
-    async execute(msg) {
-        msg.reply('Commander, if you need help planning for battle, use this ➜ https://lootandwaifus.com/nikke-team-builder/')
+    async execute(interaction) {
+        interaction.reply({ content: 'Commander, if you need help planning for battle, use this ➜ https://lootandwaifus.com/nikke-team-builder/', ephemeral: true })
     }
 }
