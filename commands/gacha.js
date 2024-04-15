ea0316684516843be504eec668c7e506b0bc9595
James Abraham <Jamesabraha@gmail.com>
2024-04-14 19:38:05 -0600

    feat: Add gacha functionality

---
+++ commands/gacha.js
@@ -0,0 +1,72 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const charactersData = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'utils', 'characters.json'), 'utf-8'));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gacha')
+        .setDescription('Performs a gacha pull for NIKKE!')
+        .addStringOption(option =>
+            option.setName('type')
+                .setDescription('Type of pull')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'single', value: 'single' },
+                    // TODO: Disabled until we figure out how to make this shit look pretty in discord embeds...
+                    // { name: 'multi', value: 'multi' } 
+                )),
+
+    async execute(interaction) {
+        const pullType = interaction.options.getString('type');
+        const results = pullCharacters(pullType);
+        const embeds = generateEmbeds(results);
+        if (embeds.length > 10) { // Discord allows a max of 10 embeds per message
+            for (let i = 0; i < embeds.length; i += 10) {
+                await interaction.followUp({ embeds: embeds.slice(i, i + 10) });
+            }
+        } else {
+            await interaction.reply({ embeds: embeds, em });
+        }
+    }
+};
+
+function pullCharacters(pullType) {
+    const SSR_RATE = 0.04;
+    const SR_RATE = 0.43;
+    const R_RATE = 0.53;
+    const results = [];
+    const pulls = pullType === 'multi' ? 10 : 1;
+
+    for (let i = 0; i < pulls; i++) {
+        const rand = Math.random();
+        let rarity;
+        let characterPool;
+
+        if (rand < SSR_RATE) {
+            rarity = 'SSR';
+            characterPool = charactersData.SSR;
+        } else if (rand < SSR_RATE + SR_RATE) {
+            rarity = 'SR';
+            characterPool = charactersData.SR;
+        } else {
+            rarity = 'R';
+            characterPool = charactersData.R;
+        }
+
+        const character = characterPool[Math.floor(Math.random() * characterPool.length)];
+        results.push({ ...character, rarity });
+    }
+
+    return results;
+}
+
+function generateEmbeds(characters) {
+    return characters.map(char => {
+        return new EmbedBuilder()
+            .setTitle(`${char.name} - Rarity: ${char.rarity}`)
+            .setColor(0x00AE86)
+            .setImage(char.image)
+            .setFooter({ text: 'Gacha results' });
+    });
+}
