4aed51d346ab41e8ca5f1a215e423c516524a2d5
Sefhi <echo.mascarell@gmail.com>
2024-04-21 18:34:00 +0200

    updated list of commands

--- commands/help.js
+++ commands/help.js
@@ -7,8 +7,10 @@ module.exports = {
         .setDescription('List of custom commands available for all Commanders'),
     async execute(msg) {
 			msg.reply({ content: `CUSTOM COMMANDS \n 
+➜ **/gacha <type> <game>** : Gamba with rapi bot, you can do singles or multis, works with NIKKE and Solo Leveling: Arise
 ➜ **/<name of Nikke> list** : get full list of correct advice answers of a Nikke in NIKKE
 ➜ **/<name of Nikke> <term>** : get correct advice answers when typing the nikke name and search term for advice in NIKKE
+➜ **/lucky** : How lucky are you today?
 ➜ **damn train** : Fuck Thomas The Train
 ➜ **damn gravedigger** : Time for OSU!
 ➜ **good girl** : say thanks to the best girl & bot in this server 
